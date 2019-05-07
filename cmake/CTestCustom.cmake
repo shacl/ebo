0bcf33399a1e08053b20db0821c149592566c90d
Austin McCartney <amccartney@lanl.gov>
2019-05-07 09:48:17 -0600

    addition coverage exclusion

--- cmake/CTestCustom.cmake
+++ cmake/CTestCustom.cmake
@@ -1,6 +1,7 @@
 list(APPEND CTEST_CUSTOM_COVERAGE_EXCLUDE
   ".*catch.*"
   ".*test.*"
-  ".*c[+][+]")
+  ".*c[+][+]"
+  ".*shacl/trait.*hpp")
 
 list(APPEND CTEST_EXTRA_COVERAGE_GLOB ".*shacl/ebo.*hpp")
